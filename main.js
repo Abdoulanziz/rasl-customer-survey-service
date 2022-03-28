5ddaf0913c7d59839bee2550119491f8db7d9d60
Abdoulanziz Ally <35191747+Abdoulanziz@users.noreply.github.com>
2022-03-28 16:55:58 +0300

    From office

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const rows = document.querySelectorAll('.row');
     rows[1].remove();
     rows[2].remove();
+    rows[3].remove();
+    rows[4].remove();
 
     setTimeout(()=>{
         rows[0].remove();
@@ -12,8 +14,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
             e.preventDefault();
             rows[1].remove();
             document.querySelector('form').append(rows[2]);
+
+            const ans = document.querySelectorAll('.an');
+            ans.forEach(an => {
+                an.addEventListener('click', ()=> {
+                    rows[2].remove();
+                    document.querySelector('form').append(rows[3]);
+
+                    const nums = document.querySelectorAll('.star');
+                    nums.forEach(num => {
+                        num.addEventListener('click', ()=> {
+                            rows[3].remove();
+                            document.querySelector('form').append(rows[4]);
+                        });
+                    });
+                });
+            });
         });
+
+        
+
     }, 2000);
+
+    
+
+    
+
+    
 
 
 
