6e7d5cd03af2c8bcb445d857a178c8105a7a1bda
Abdoulanziz Ally <35191747+Abdoulanziz@users.noreply.github.com>
2022-04-05 15:47:00 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    const ratings = document.querySelectorAll('.ans .an.star');
+    ratings[1].addEventListener('click', ()=>{
+        console.log(ratings);
+        ratings[0].classList.add('.selected');
+        ratings[1].classList.add('.selected');
+    });
+    
 });
